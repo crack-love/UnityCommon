fab5240df0c6c2723014c36c9560991f0fc60a94
김용겸 <masterchip@naver.com>
2022-03-26 18:34:21 +0900

    SerializableDictionary

---
+++ Assets/Common/Runtime/Scripts/Serialization/UnitySerialize/SerializableDictionary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    [Serializable]
+    public class SerializableDictionary<TK, TV> : Dictionary<TK, TV>, ISerializationCallbackReceiver
+    {
+        [Serializable]
+        struct KeyValuePair
+        {
+            public TK Key;
+            public TV Value;
+        }
+
+        [SerializeField] KeyValuePair[] m_serializedPairs;
+
+#if UNITY_EDITOR
+        [SerializeField] bool m_deserializationFail;
+#endif
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            if (m_deserializationFail) return;
+
+            var count = Count;
+
+            // reallocate
+            if (m_serializedPairs == null ||
+                m_serializedPairs.Length != count)
+            {
+                m_serializedPairs = new KeyValuePair[count];
+            }
+
+            // set serializeds
+            int idx = 0;
+            foreach (var pair in this)
+            {
+                m_serializedPairs[idx].Key = pair.Key;
+                m_serializedPairs[idx++].Value = pair.Value;
+            }
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            Clear();
+            m_deserializationFail = false;
+
+            // add items
+            int size = m_serializedPairs.Length;
+            for (int i = 0; i < size; ++i)
+            {
+                if (ContainsKey(m_serializedPairs[i].Key))
+                {
+                    m_deserializationFail = true;
+                    return;
+                }
+
+                Add(m_serializedPairs[i].Key, m_serializedPairs[i].Value);
+            }
+        }
+    }
+}
