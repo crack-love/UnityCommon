ad4dc6089631ed3f7f0ef856a95fc031ee3b5dce
KYG_Ideapad <masterchip@naver.com>
2022-08-09 22:39:37 +0900

    add TServ Type for Singletone

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
    - serializable hashset, que, randomqueue, stack, transform
    - (dictinary 제외 모든 코드)
 1. common -> scripts -> needreview dir 이하 모든 코드
+1. divide concurrent code
--- Scripts/Generics/MonobehaviourSingletone.cs
+++ Scripts/Generics/MonobehaviourSingletone.cs
@@ -2,34 +2,93 @@ using UnityEngine;
 
 namespace UnityCommon
 {
-    public class MonobehaviourSingletone<T> : MonoBehaviour
-        where T : MonoBehaviour
+    public class MonobehaviourSingletone<TDerived> : MonoBehaviour
+        where TDerived : MonobehaviourSingletone<TDerived>
     {
-        protected static object m_mutex;
-        static T m_instance;
+        protected static object m_singletoneMutex;
+        static TDerived m_instance;
 
         static MonobehaviourSingletone()
         {
-            m_mutex = new object();Debug.Log("Mutex " + typeof(T).FullName);
+            m_singletoneMutex = new object();
         }
 
-        public static T Instance
+        public static TDerived Instance
         {
             get
             {
                 if (!m_instance)
                 {
-                    lock (m_mutex)
+                    lock (m_singletoneMutex)
                     {
                         if (!m_instance)
                         {
-                            m_instance = FindObjectOfType<T>();
+                            m_instance = FindObjectOfType<TDerived>();
+
+                            if (!m_instance)
+                            {
+                                string tname = typeof(TDerived).FullName;
+                                Debug.LogError("Can't Find Singletone Object " + tname);
+                            }
                         }
                     }
                 }
 
                 return m_instance;
             }
+        }
+
+        protected void SetInstance(TDerived instance)
+        {
+            m_instance = instance;
+        }
+    }
+
+    public class MonobehaviourSingletone<TDerived, TServ> : MonoBehaviour
+        where TDerived : MonoBehaviour, TServ
+    {
+        protected static object m_singletoneMutex;
+        static TServ m_instance;
+
+        static MonobehaviourSingletone()
+        {
+            m_singletoneMutex = new object();
+        }
+
+        public static TServ Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    lock (m_singletoneMutex)
+                    {
+                        if (m_instance == null)
+                        {
+                            TDerived found = FindObjectOfType<TDerived>();
+
+                            // check bool operation with TDerived type not TServ which will be assinged.
+                            // for prevent malfunction that destroyed unity object will return false but
+                            // can be assigned and it will not null
+                            if (!found)
+                            {
+                                string tname = typeof(TDerived).FullName;
+                                Debug.LogError("Can't Find Singletone Object " + tname);
+                            }
+                            else
+                            {
+                                m_instance = found;
+                            }
+                        }
+                    }
+                }
+
+                return m_instance;
+            }
+        }
+        protected void SetInstance(TDerived instance)
+        {
+            m_instance = instance;
         }
     }
 }
